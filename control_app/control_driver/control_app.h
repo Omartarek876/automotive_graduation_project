5782aef00545edc5bf258546cb12a67a6391267d
omar tarek <ot482002@gmail.com>
2024-11-21 00:54:52 +0200

    add the main functions of motion

---
+++ control_app/control_driver/control_app.h
@@ -0,0 +1,22 @@
+/*
+ * control_app.h
+ *
+ *  Created on: Nov 21, 2024
+ *      Author: Omar Tarek
+ */
+
+#ifndef CONTROL_DRIVER_CONTROL_APP_H_
+#define CONTROL_DRIVER_CONTROL_APP_H_
+
+#define NUMBER_OF_ITERATIONS_PER_ONE_MILI_SECOND 364
+
+void Delay_MS(unsigned long long n);
+void car_init (void);
+void car_forword (void);
+void car_backword (void);
+void car_right (void);
+void car_left (void);
+void car_stop (void);
+
+
+#endif /* CONTROL_DRIVER_CONTROL_APP_H_ */

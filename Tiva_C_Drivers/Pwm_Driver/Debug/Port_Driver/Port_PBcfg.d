9a79f4747f06ba2dd403e836f8fdab56410c573a
Mahmoud Enany <157853813+Mahmoud-Enany@users.noreply.github.com>
2024-11-22 00:50:12 +0200

    drivers(pwm): implment pwm drivers

---
+++ Tiva_C_Drivers/Pwm_Driver/Debug/Port_Driver/Port_PBcfg.d
@@ -0,0 +1,24 @@
+# FIXED
+
+Port_Driver/Port_PBcfg.obj: ../Port_Driver/Port_PBcfg.c
+Port_Driver/Port_PBcfg.obj: ../Port_Driver/Port.h
+Port_Driver/Port_PBcfg.obj: ../Critical_Files/Std_Types.h
+Port_Driver/Port_PBcfg.obj: ../Critical_Files/Platform_Types.h
+Port_Driver/Port_PBcfg.obj: ../Critical_Files/Compiler.h
+Port_Driver/Port_PBcfg.obj: ../Port_Driver/Port_cfg.h
+Port_Driver/Port_PBcfg.obj: ../Critical_Files/Common_Macros.h
+
+../Port_Driver/Port_PBcfg.c:
+
+../Port_Driver/Port.h:
+
+../Critical_Files/Std_Types.h:
+
+../Critical_Files/Platform_Types.h:
+
+../Critical_Files/Compiler.h:
+
+../Port_Driver/Port_cfg.h:
+
+../Critical_Files/Common_Macros.h:
+
---
+++ Tiva_C_Drivers/Pwm_Driver/Debug/main.d
@@ -0,0 +1,90 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: ../PWM_Channel_10kHz/Clock_Driver.h
+main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h
+main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h
+main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h
+main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h
+main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h
+main.obj: ../Critical_Files/Private_registers.h
+main.obj: ../Critical_Files/Platform_Types.h
+main.obj: ../PWM_Channel_10kHz/PWM_Driver.h
+main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/math.h
+main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_defs.h
+main.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_limits.h
+main.obj: ../PWM_Channel_10kHz/Debug_Driver.h
+main.obj: ../Dio_Driver/Dio.h
+main.obj: ../Critical_Files/Std_Types.h
+main.obj: ../Critical_Files/Compiler.h
+main.obj: ../Dio_Driver/Dio_Cfg.h
+main.obj: ../Critical_Files/Common_Macros.h
+
+../main.c:
+
+../PWM_Channel_10kHz/Clock_Driver.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/string.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/xlocale/_string.h:
+
+../Critical_Files/Private_registers.h:
+
+../Critical_Files/Platform_Types.h:
+
+../PWM_Channel_10kHz/PWM_Driver.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/math.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_defs.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_limits.h:
+
+../PWM_Channel_10kHz/Debug_Driver.h:
+
+../Dio_Driver/Dio.h:
+
+../Critical_Files/Std_Types.h:
+
+../Critical_Files/Compiler.h:
+
+../Dio_Driver/Dio_Cfg.h:
+
+../Critical_Files/Common_Macros.h:
+

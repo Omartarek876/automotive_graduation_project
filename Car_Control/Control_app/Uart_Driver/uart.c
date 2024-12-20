7e205cb7d78f80ddeb4ed37d7c78609c32fcb96c
omar tarek <ot482002@gmail.com>
2024-12-20 19:47:24 +0200

    update control app with the serial communicaion with ESP32

--- Car_Control/Control_app/Uart_Driver/uart.c
+++ Car_Control/Control_app/Uart_Driver/uart.c
@@ -18,15 +18,21 @@
 #include "Critical_Files/Platform_Types.h"
 #include "Critical_Files/Private_registers.h"
 
-// Global pointer to hold the base address of the UART module
+
 volatile uint32* uart_ptr = NULL_PTR ;
+volatile uint32* uart_bases[8] = {
+    (volatile uint32*)UART0_BASE_ADDRESS,
+    (volatile uint32*)UART1_BASE_ADDRESS,
+    (volatile uint32*)UART2_BASE_ADDRESS,
+    (volatile uint32*)UART3_BASE_ADDRESS,
+    (volatile uint32*)UART4_BASE_ADDRESS,
+    (volatile uint32*)UART5_BASE_ADDRESS,
+    (volatile uint32*)UART6_BASE_ADDRESS,
+    (volatile uint32*)UART7_BASE_ADDRESS
+};
 
-
-// Function to initialize the UART module based on user configuration
   void uartInit(uart_configuration* ptr2config)
 {
-
-// Select the base address of the UART module based on the uart_num provided
   switch(ptr2config->uart_num)
   {
   case 0:
@@ -55,7 +61,6 @@ volatile uint32* uart_ptr = NULL_PTR ;
     break;
  }
 
-// Enable the clock for the selected UART module
   SET_BIT(SYSCTL_RCGCUART_REG,ptr2config->uart_num) ;
 
 
@@ -90,112 +95,153 @@ volatile uint32* uart_ptr = NULL_PTR ;
              break;
      }
 
-    // Disable UART module before configuring it
   CLEAR_BIT(*(volatile uint32*)((volatile uint8 *)uart_ptr + UART_CTRL_REG_OFFSET),FIRST_BIT);
 
-    // Calculate and configure integer and fractional baud rate
-  float32 val = (float32)(ptr2config->clk)/(float32)(16*ptr2config->baud_rate) ;
-  uint32 int_val = (uint32) val ; // Integer part of baud rate
-  float32 float_val = (float32)val - (float32)int_val  ; // Fractional part
-
-   // Convert fractional part to a register value (scaled by 64)
-  uint32 float_reg = (uint32)(float_val*64.0 + 0.5) ;
-
-  // Set the integer baud rate
+  float32 val = (float32)(ptr2config->clk)/(float32)(16*ptr2config->baud_rate) ; 
+  uint32 int_val = (uint32) val ; 
+  float32 float_val = (float32)val - (float32)int_val  ; 
+  
+  uint32 float_reg = (uint32)(float_val*64.0 + 0.5) ; 
   *(volatile uint32 *)((volatile uint8 *)uart_ptr + UART_INTEGER_BAUD_RATE_REG_OFFSET) = ((*(volatile uint32 *)((volatile uint8 *)uart_ptr + UART_INTEGER_BAUD_RATE_REG_OFFSET)) & \
     (UART_INTEGER_BAUD_RATE_MASK)) | (int_val << INTDIV_IBRD_REG_START_BIT);
-
-
-  // Set the fractional baud rate
+  
+  
+  
   *(volatile uint32 *)((volatile uint8 *)uart_ptr + UART_FRACTIONAL_BAUD_RATE_REG_OFFSET) = ((*(volatile uint32 *)((volatile uint8 *)uart_ptr + UART_FRACTIONAL_BAUD_RATE_REG_OFFSET)) & \
     (UART_FRACTIONAL_BAUD_RATE_MASK)) | (float_reg << FLOAT_DIV_FBRD_REG_START_BIT);
-
-  // Configure the clock source for the UART
+  
   *(volatile uint32 *)((volatile uint8 *)uart_ptr + UART_CLOCK_SOURCE_REG_OFFSET) = ((*(volatile uint32 *)((volatile uint8 *)uart_ptr + UART_CLOCK_SOURCE_REG_OFFSET)) & \
     (UART_CLOCK_SOURCE_MASK)) | (UART_CLK_FREQUENCY_MASK);
-
-  // Enable UART module after configuration
+  
   SET_BIT(*(volatile uint32 *)((volatile uint8 *)uart_ptr + UART_CTRL_REG_OFFSET),UART_CTL_ENABLE_BIT);
 
-  // Set the data length (e.g., 8-bit, 9-bit) in the Line Control Register
+  
   *(volatile uint32 *)((volatile uint8 *)uart_ptr + UART_LINE_CTRL_REG_OFFSET) = ((*(volatile uint32 *)((volatile uint8 *)uart_ptr + UART_LINE_CTRL_REG_OFFSET)) & \
     (UART_LINE_CTRL_SOURCE_MASK)) | (ptr2config->length << LENGTH_LINE_CTRL_REG_START_BIT);
-
+  
 
 }
 
-// Function to send a byte via UART
+/*
 void UART_sendByte(const uint8 data)
 {
-    // Wait until UART is ready to transmit (check the Transmit Flag)
    while(((*(volatile uint32 *)((volatile uint8 *)uart_ptr + UART_FLAG_RATE_REG_OFFSET)) & (1 << TRANSMIT_FLAG_BIT)) != 0);
-
-    // Write the data to the UART Data Register for transmission
+  
   *(volatile uint32 *)((volatile uint8 *)uart_ptr + UART_DATA_REG_OFFSET) = data;
 }
-
-// Function to receive a byte via UART
+*/
+  void UART_sendByte(uint8 uart_num, const uint8 data)
+  {
+      volatile uint32* u_ptr = uart_bases[uart_num];
+      while(((*(volatile uint32 *)((volatile uint8 *)u_ptr + UART_FLAG_RATE_REG_OFFSET)) & (1 << TRANSMIT_FLAG_BIT)) != 0);
+      *(volatile uint32 *)((volatile uint8 *)u_ptr + UART_DATA_REG_OFFSET) = data;
+  }
+/*
 uint8 uart_RecieveByte(void)
 {
   uint8 recieved_byte;
-
-// Wait until UART has received data (check the Receive Flag)
   while(((*(volatile uint32 *)((volatile uint8 *)uart_ptr + UART_FLAG_RATE_REG_OFFSET)) & (1 << RECIEVE_FLAG_BIT)) != 0);
-
-// Read the received byte from the UART Data Register
   recieved_byte =  *(volatile uint32 *)((volatile uint8 *)uart_ptr + UART_DATA_REG_OFFSET);
   return recieved_byte;
 }
-
-
+*/
+  uint8 uart_RecieveByte(uint8 uart_num)
+  {
+      volatile uint32* u_ptr = uart_bases[uart_num];
+      uint8 recieved_byte;
+      while(((*(volatile uint32 *)((volatile uint8 *)u_ptr + UART_FLAG_RATE_REG_OFFSET)) & (1 << RECIEVE_FLAG_BIT)) != 0);
+      recieved_byte =  *(volatile uint32 *)((volatile uint8 *)u_ptr + UART_DATA_REG_OFFSET);
+      return recieved_byte;
+  }
+/*
 void UART_SendString(const uint8 *pData)
 {
     uint32 uCounter =0;
-    /* Transmit the whole string */
     while(pData[uCounter] != '\0')
     {
-        UART_sendByte(pData[uCounter]); /* Send the byte */
-        uCounter++; /* increment the counter to the next byte */
+        UART_sendByte(pData[uCounter]);
+        uCounter++;
     }
 }
+*/
+void UART_SendString(uint8 uart_num, const uint8 *pData)
+{
+    uint32 uCounter = 0;
 
-
+    /* Transmit the whole string */
+    while (pData[uCounter] != '\0')
+    {
+        UART_sendByte(uart_num, pData[uCounter]);
+        uCounter++;
+    }
+}
+/*
 void UART_ReceiveString(uint8 *pData)
 {
     uint32 uCounter =0;
 
-    pData[uCounter] = uart_RecieveByte(); /* Receive the first byte */
+    pData[uCounter] = uart_RecieveByte();
 
-    /* Receive the whole string until the '#' */
     while(pData[uCounter] != '#')
     {
         uCounter++;
         pData[uCounter] = uart_RecieveByte();
     }
-    /* After receiving the whole string plus the '#', replace the '#' with '\0' */
     pData[uCounter] = '\0';
 }
+*/
+void UART_ReceiveString(uint8 uart_num, uint8 *pData)
+{
+    uint32 uCounter = 0;
 
+    pData[uCounter] = uart_RecieveByte(uart_num);
 
+    while (pData[uCounter] != '#')
+    {
+        uCounter++;
+        pData[uCounter] = uart_RecieveByte(uart_num);
+    }
+
+    pData[uCounter] = '\0';
+}
+/*
 void UART_SendData(const uint8 *pData, uint32 uSize)
 {
-    /* Transmit the number of bytes requested on the UART port */
     while(uSize--)
     {
 
-        UART_sendByte(*pData); /* Send the byte */
-        pData++; /* increment pointer to the next byte */
+        UART_sendByte(*pData) ;
+        pData++;
     }
 }
-
-
+*/
+void UART_SendData(uint8 uart_num, const uint8 *pData, uint32 uSize)
+{
+    /* Transmit the number of bytes requested on the UART port */
+    while (uSize--)
+    {
+        UART_sendByte(uart_num, *pData);
+        pData++;
+    }
+}
+/*
 void UART_ReceiveData(uint8 *pData, uint32 uSize)
 {
-    /* Receive the number of bytes requested on the UART port */
+    // Receive the number of bytes requested on the UART port
     while(uSize--)
     {
-        *pData = uart_RecieveByte(); /* receive a byte from the UART */
-         pData++; /* increment pointer to receive the next byte in the next location */
+        *pData = uart_RecieveByte(); // receive a byte from the UART
+         pData++; //increment pointer to receive the next byte in the next location
+    }
+}
+*/
+void UART_ReceiveData(uint8 uart_num, uint8 *pData, uint32 uSize)
+{
+    /* Receive the number of bytes requested on the UART port */
+    while (uSize--)
+    {
+        *pData = uart_RecieveByte(uart_num);
+        pData++;
     }
 }
 

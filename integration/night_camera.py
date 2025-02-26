c4305b7907b600f8ea8893fc57a455772fbb005e
mahmoudsamir <ms7582772@gmail.com>
2025-02-26 20:10:05 +0200

    The  code of integration of the two systems is added

---
+++ integration/night_camera.py
@@ -0,0 +1,29 @@
+import cv2
+from ultralytics import YOLO
+
+
+model = YOLO("night.pt") 
+
+
+cap = cv2.VideoCapture(1) 
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    
+    results = model(frame)
+
+    
+    annotated_frame = results[0].plot()
+
+   
+    cv2.imshow("NightVision System", annotated_frame)
+
+  
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
